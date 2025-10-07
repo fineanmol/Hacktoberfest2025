cd90d1ead93ec8bdec9c7f54f40eb6f356c5274b
Ishita Bairagi <152215603+Berrita2004@users.noreply.github.com>
2025-10-08 00:44:44 +0530

    Update Frequency of the Most Frequent Element.java

--- Frequency of the Most Frequent Element.java
+++ Frequency of the Most Frequent Element.java
@@ -5,7 +5,6 @@ class Solution {
         Arrays.sort(nums);
         long total = 0;
         int left = 0, res = 1;
-
         for (int right = 0; right < nums.length; right++) {
             total += nums[right];
             // If making all elements in window [left, right] equal to nums[right] costs more than k, move left pointer
