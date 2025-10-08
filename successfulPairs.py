10ad278251edec24c96e02c7024cbc7801c1593a
Alen Thomas <133691975+AIstar007@users.noreply.github.com>
2025-10-09 01:36:00 +0530

    Update successfulPairs.py

--- successfulPairs.py
+++ successfulPairs.py
@@ -1,24 +1,13 @@
-class Solution {
-public:
-    static vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
-        int freq[100001]={0}, pMax=0;
-        for(int p: potions){
-            freq[p]++;
-            pMax=max(pMax, p);
-        }
-        // freq = prefix sum for freq 
-        partial_sum( freq, freq+pMax+1, freq);
-
-        const int n=spells.size(), m=potions.size();
-        vector<int> result(n, 0);
-        
-        for (int i=0; i<n; i++) {
-            const int spell=spells[i];
-            const long long k = (success+spell-1)/spell;
-            if (k<=pMax) {
-                result[i]=m-(k>=1?freq[k-1]:0);
-            }
-        }       
-        return result;
-    }
-};
+class Solution:
+    def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+        potions.sort()
+        potion_count = len(potions)
+        result = []
+        for spell_power in spells:
+            required_strength = math.ceil(success / spell_power)
+            if required_strength > potions[-1]:
+                result.append(0)
+                continue
+            index = bisect.bisect_left(potions, required_strength)
+            result.append(potion_count - index)
+        return result
