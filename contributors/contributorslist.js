7f86da361d65ab8f67be54560b0c18eef6394213
Shresth Juyal <shresth.j25436@nst.rishihood.edu.in>
2025-10-10 15:23:17 +0530

    Added Name to Contributor

--- contributors/contributorslist.js
+++ contributors/contributorslist.js
@@ -2712,4 +2712,10 @@ contributors = [
     fullname: "Pratishta Holkar",
     username: "https://github.com/Pratishta08",
   },
+
+   {
+    id: 476,
+    fullname: "Shresth Juyal",
+    username: "https://github.com/Shresth-14",
+  },
 ];
