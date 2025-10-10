50f1c2d91e10a7c01220866203f1adb5be304b5c
Jabrail Shahbazov <j.shahbazov@tayqatech.com>
2025-10-11 00:42:06 +0400

    added new alian name app

---
+++ CSharp/AlienNameGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class AlienNameGenerator
+{
+    static void Main()
+    {
+        string[] prefixes = { "Zor", "Xan", "Qel", "Vro", "Neb", "Glu" };
+        string[] middles = { "thar", "ix", "ul", "om", "ar", "eq" };
+        string[] suffixes = { "on", "ax", "oid", "ar", "ium", "ex" };
+
+        Random rnd = new Random();
+        Console.WriteLine("Welcome to the Alien Name Generator!\n");
+
+        for (int i = 0; i < 10; i++)
+        {
+            string alienName = prefixes[rnd.Next(prefixes.Length)]
+                               + middles[rnd.Next(middles.Length)]
+                               + suffixes[rnd.Next(suffixes.Length)];
+
+            Console.WriteLine($"Alien #{i + 1}: {alienName}");
+        }
+
+        Console.WriteLine("\nPress Enter to exit...");
+        Console.ReadLine();
+    }
+}
