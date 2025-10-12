b25378786911d23cc1867321fa7bef5f4562b2c8
Prince <167101466+pkprajapati7402@users.noreply.github.com>
2025-10-12 14:22:25 +0530

    Add Prince Kumar Prajapati to contributors list

--- contributors/contributorsList1.js
+++ contributors/contributorsList1.js
@@ -2726,4 +2726,9 @@ contributors = [
     fullname: "Shreya Pal",
     username: "https://github.com/PLayerNightQueen16",
   },
+  {
+    id: 476,
+    fullname: "Prince Kumar Prajapati",
+    username: "https://github.com/pkprajapati7402",
+  },
 ];
