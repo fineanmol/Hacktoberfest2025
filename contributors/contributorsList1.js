c1971dcd19bf7d7a96d2c3799bde5697ee8ce6bf
Sri Ram <118582602+Sriramm-15@users.noreply.github.com>
2025-10-13 18:55:55 +0530

    Update contributorsList1.js
    Added my name into the contribution list

--- contributors/contributorsList1.js
+++ contributors/contributorsList1.js
@@ -2726,4 +2726,10 @@ contributors = [
     fullname: "Shreya Pal",
     username: "https://github.com/PLayerNightQueen16",
   },
+  
+  { 
+    id:476,
+    fullname:"Sri Ram Reddy Alla" ,
+    username: "https://github.com/Sriramm-15"
+  }
 ];
