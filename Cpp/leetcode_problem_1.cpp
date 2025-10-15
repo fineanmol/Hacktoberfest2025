813ad28b07a6384402a722a6e611da2313d171b8
JyotiSaxena1 <jyoti.230101064@iiitbh.ac.in>
2025-10-15 19:48:58 +0530

    Added leetcode problem for Adjacent Increasing 2

--- Cpp/leetcode_problem_1.cpp
+++ Cpp/leetcode_problem_1.cpp
@@ -4,16 +4,16 @@ class Solution {
 public:
     bool hasIncreasingSubarrays(vector<int>& nums, int k) {
         int n = nums.size();
-        int cnt = 1, precnt = 0, ans = 0;
+        int cnt = 1, precont = 0, ans = 0;
         for (int i = 1; i < n; ++i) {
             if (nums[i] > nums[i - 1]) {
                 ++cnt;
             } else {
-                precnt = cnt;
+                precont = cnt;
                 cnt = 1;
             }
-            ans = max(ans, min(precnt, cnt));
-            ans = max(ans, cnt / 2);
+            ans =max(ans, min(precont, cnt));
+            ans =max(ans, cnt / 2);
         }
         return ans >= k;
     }
