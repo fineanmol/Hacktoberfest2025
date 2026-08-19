3637a8a3edf7d3985f8aac67232c9903b40a82b2
DanishBasha <danishbasha18@gmail.com>
2026-08-19 14:23:47 +0530

    pull request

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1906,4 +1906,9 @@ contributors = [
     fullname: "Prajwal Ramgond",
     username: "https://github.com/PRAJWAL-RAMGOND",
   },
+  {
+    id: 376,
+    fullname: "Danish Basha",
+    username: "https://github.com/DanishBasha",
+  },
 ];
